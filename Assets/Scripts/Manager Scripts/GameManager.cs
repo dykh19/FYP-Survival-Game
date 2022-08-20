56fa3df5e3ac082690717e5d3ead1b5edc1fc75f
dykh19 <dykh19@gmail.com>
2022-08-20 14:56:53 +0800

    Gun Reload and Shoot

--- Assets/Scripts/Manager Scripts/GameManager.cs
+++ Assets/Scripts/Manager Scripts/GameManager.cs
@@ -1,29 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// Game Manager is in-charge of the overall game state
 public class GameManager : MonoBehaviour
 {
-    public enum Difficulty
-    {
-        EASY,
-        NORMAL,
-        HARD,
-        ENDLESS
-    }
-
-    public enum GameMode
-    {
-        NORMAL,
-        ENDLESS
-    }
-
     public static GameManager Instance { get; private set; }
-    public GameMode CurrentMode;
+    public GameMode CurrentGameMode;
     public Difficulty CurrentDifficulty;
+    public GameState CurrentGameState;
 
     void Awake()
     {
+        // Make sure there is only one instance of GameManager and prevent it from being destroyed
         if (Instance != null && Instance != this)
         {
             Destroy(this);
@@ -44,16 +32,49 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("escape"))
+        {
+            if(CurrentGameState == GameState.INGAME)
+            {
+                CurrentGameState = GameState.PAUSED;
+                PauseGame();
+            }
+            else if(CurrentGameState == GameState.PAUSED)
+            {
+                CurrentGameState = GameState.INGAME;
+                ResumeGame();
+            }    
+
+        }
     }
 
     public void SetGameMode(int newMode)
     {
-        CurrentMode = (GameMode)newMode;
+        CurrentGameMode = (GameMode)newMode;
     }
 
     public void SetDifficulty(int newDifficulty)
     {
         CurrentDifficulty = (Difficulty)newDifficulty;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void WinGame()
+    {
+
+    }
+
+    public void LoseGame()
+    {
+
     }
 }
